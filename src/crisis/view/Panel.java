bbde1807dcccfa7447c31660df9590e288a50af2
Caleb Cole <calebcole121@gmail.com>
2022-04-27 08:29:47 -0600

    Added the layouts for initial buttons.

--- src/crisis/view/Panel.java
+++ src/crisis/view/Panel.java
@@ -23,6 +23,19 @@ public class Panel extends JPanel
 		this.button4 = new JButton("UPDATE");
 		
 		this.layout = new SpringLayout();
+		layout.putConstraint(SpringLayout.NORTH, button4, 0, SpringLayout.NORTH, health);
+		layout.putConstraint(SpringLayout.WEST, button4, 19, SpringLayout.EAST, button3);
+		layout.putConstraint(SpringLayout.SOUTH, button4, 0, SpringLayout.SOUTH, health);
+		layout.putConstraint(SpringLayout.NORTH, button3, 0, SpringLayout.NORTH, health);
+		layout.putConstraint(SpringLayout.WEST, button3, 18, SpringLayout.EAST, button2);
+		layout.putConstraint(SpringLayout.SOUTH, button3, 0, SpringLayout.SOUTH, health);
+		layout.putConstraint(SpringLayout.NORTH, button2, 0, SpringLayout.NORTH, health);
+		layout.putConstraint(SpringLayout.WEST, button2, 15, SpringLayout.EAST, health);
+		layout.putConstraint(SpringLayout.SOUTH, button2, 0, SpringLayout.SOUTH, health);
+		layout.putConstraint(SpringLayout.WEST, health, 9, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.NORTH, health, -69, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.SOUTH, health, -10, SpringLayout.SOUTH, this);
+		
 		
 		setupPanel();
 		setupLayout();
