a75022bbde25318489253c90ff7f3659f2dcbc60
Braden Byrnes <mattbyrnes18@gmail.com>
2022-04-29 09:01:17 -0600

    added set up for events

--- src/crisis/model/ExistentialCrisis.java
+++ src/crisis/model/ExistentialCrisis.java
@@ -1,12 +1,24 @@
 package crisis.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import crisis.view.Popup;
+
 public class ExistentialCrisis 
 {	
+	private Popup pop;
 	private String name;
 	private int sanity;
 	private int hunger;
 	private int time;
 	private int day;
+	private int fileName;
+	private String[] event;
+	private ArrayList <String> eventData;
 	
 	public ExistentialCrisis()
 	{
@@ -15,6 +27,10 @@ public class ExistentialCrisis
 		this.hunger = 100;
 		this.time = 0;
 		this.day = 0;
+		this.pop = new Popup();
+		this.event = new String[9];
+		this.fileName = 1;
+		this.eventData = new ArrayList<String>();
 	}
 	
 	/**
@@ -24,17 +40,17 @@ public class ExistentialCrisis
 	{
 		switch(time)
 		{
-		case 3:
-			food();
-			break;
-			
-		case 5:
-			sleep();
-			break;
-			
-		default:
-			startEvent();
-			break;
+			case 3:
+				food();
+				break;
+				
+			case 5:
+				sleep();
+				break;
+				
+			default:
+				startEvent();
+				break;
 		}
 	}
 	
@@ -166,7 +182,9 @@ public class ExistentialCrisis
 	 */
 	public void startEvent()
 	{
-		
+		fileName = gameRandom(1);
+		eventData = loadTextToList(fileName + ".txt");
+		event = (String[]) eventData.toArray();
 	}
 	
 	/**
@@ -185,4 +203,69 @@ public class ExistentialCrisis
 		
 	}
 			
+	/**
+	 * saves player responses and then prints them to a list
+	 * @param responses
+	 * @param filename
+	 */
+	private void saveListAsText(ArrayList <String> responses, String filename)
+	{
+		File saveFile = new File(filename);
+		
+		try (PrintWriter saveText = new PrintWriter(saveFile))
+		{
+			for (String content : responses)
+			{
+				saveText.println(content);
+			}
+		}
+		catch(IOException errorFromIO)
+		{
+			handleError(errorFromIO);
+		}
+		catch(Exception genericError) 
+		{
+			handleError(genericError);
+		}
+	}
+	
+	/**
+	 * takes information from a file and loads it into a list
+	 * @param filename
+	 * @return an arraylist of what was in the file
+	 */
+	private ArrayList<String> loadTextToList(String filename)
+	{
+		ArrayList<String> fileContents = new ArrayList<String>();
+		
+		File source = new File(filename);
+		
+		try(Scanner fileScanner = new Scanner(source))
+		{
+			while (fileScanner.hasNextLine())
+			{
+				fileContents.add(fileScanner.nextLine());
+			}
+		}
+		catch(IOException fileError)
+		{
+			handleError(fileError);
+		}
+		catch(Exception error)
+		{
+			handleError(error);
+		}
+		
+		return fileContents;
+	}
+	
+	/**
+	 * handles error for try catch
+	 * @param error
+	 */
+	public void handleError(Exception error)
+	{
+		String details = "your error is " + error.getMessage();
+		pop.displayMessage(details);
+	}
 }
