f858d894f4aeb3704b17492671b454fc7212f7a7
Braden Byrnes <mattbyrnes18@gmail.com>
2022-04-29 08:14:20 -0600

    simple event set up

--- src/crisis/model/event/Event.java
+++ src/crisis/model/event/Event.java
@@ -1,9 +1,21 @@
 package crisis.model.event;
 
+import java.util.ArrayList;
+
 public class Event 
 {
+	private String choice1;
+	private String choice2;
+	private String choice3;
+	private String choice4;
 	public void Event()
 	{
+	}
+	
+	public void displayEvent(String eventDescript, String[] eventChoices, String[] eventOutcomes)
+	{
+		//display event descript and choices
+		
 		
 	}
 }
