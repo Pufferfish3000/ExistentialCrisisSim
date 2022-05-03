771072f534875cac228a1b553019815bbd594b66
Braden Byrnes <mattbyrnes18@gmail.com>
2022-05-03 07:49:05 -0600

    fixed stack overflow error

--- src/crisis/model/event/GameEvent.java
+++ src/crisis/model/event/GameEvent.java
@@ -4,34 +4,42 @@ import java.util.ArrayList;
 
 public class GameEvent 
 {
-	private String choice1;
-	private String choice2;
-	private String choice3;
-	private String choice4;
+	private String [] data;
+	private int playerInput;
 	public void GameEvent()
 	{
+		playerInput = -1;
 	}
 	
 	/**
-	 * displays event based on parameters
+	 * displays event based on parameters and waits for player decision
 	 * @param eventDescript
 	 * @param eventChoices
 	 * @param eventOutcomes
 	 */
 	public void displayGameEvent(String[] eventData)
 	{
-		//display event descript and choices
-		System.out.println(eventData[0]);
-		System.out.println(eventData[1]);
-		System.out.println(eventData[2]);
-		System.out.println(eventData[3]);
-		System.out.println(eventData[4]);
-		System.out.println(eventData[5]);
-		System.out.println(eventData[6]);
-		System.out.println(eventData[7]);
-		System.out.println(eventData[8]);
-		System.out.println(eventData[9]);
-		System.out.println(eventData[10]);
-		System.out.println(eventData[11]);
+		data = eventData;
+		//display event data
+		switch(playerInput)
+		{
+			case 1:
+				
+				break;
+			
+			case 2:
+				break;
+			
+			case 3:
+				break;
+				
+			case 4:
+				break;
+				
+			default:
+				System.gc(); //runs garbage collector to prevent stack overflow error
+				displayGameEvent(data);
+				break;
+		}
 	}
 }
