8b9f729ce4c88fd8015e76ebd0e56b0b8c38aa9b
Braden Byrnes <mattbyrnes18@gmail.com>
2022-05-06 09:49:51 -0600

    atttempt to make jar

--- src/crisis/model/ExistentialCrisis.java
+++ src/crisis/model/ExistentialCrisis.java
@@ -237,7 +237,7 @@ public class ExistentialCrisis
 	public String startEvent()
 	{
 		fileName = gameRandom(13);
-		eventData = loadTextToList("src/crisis/model/event/" + fileName + ".txt");
+		eventData = loadTextToList("/crisis/model/event/" + fileName + ".txt");
 		eventArray = eventData.toArray(new String[0]);
 		return gameEvent.displayGameEvent(eventArray);
 	}
